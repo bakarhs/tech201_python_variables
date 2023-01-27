f4a907fd40ce571eece5c06e916b7f45cdaf378f
Bakar <bakarhs123@gmail.com>
2023-01-27 16:00:43 +0000

    learning variables

---
+++ Variables.py
@@ -0,0 +1,35 @@
+# Variables
+
+# variable_name = variable_data
+
+# a = 1 #int
+# b = 2 #int
+# c = 3.5 #float
+# hi = "Hello World!" #string
+
+# print (hi)
+# print (a)
+
+# print (1 + 2)
+# print (a + b)
+
+# the type method
+
+# print(type())
+
+# Overwriting variables
+
+# Getting user input
+
+# print("Hi, what is yout name?")
+# name = input ()
+# print("hi")
+# print(name)
+
+print("Hi, what is your name?")
+name = input()
+print ("Hi, " + name + " . How old are you?" )
+age = input()
+print("Can i also get your date of birth please?")
+DOB = input(str())
+print ("Hi, " + name + " you are " + age + " and was born on " + DOB)
